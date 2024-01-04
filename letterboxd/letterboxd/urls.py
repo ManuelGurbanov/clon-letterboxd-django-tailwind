ce2101b18c5e09dbc2fdaaad129536358ce21d38
ManuelGurbanov <manuelgurbanov@gmail.com>
2024-01-03 22:51:39 -0300

    Register

--- letterboxd/letterboxd/urls.py
+++ letterboxd/letterboxd/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from movies.views import home, add_movie, OptionalLoginView, CustomLogoutView
+from movies.views import home, add_movie, OptionalLoginView, CustomLogoutView, RegisterView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
     path('add_movie/', add_movie, name='add_movie'),
     path('login/', OptionalLoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', CustomLogoutView.as_view(), name='logout')
+    path('logout/', CustomLogoutView.as_view(), name='logout'),
+    path('register/', RegisterView.as_view(), name='register'),
 ]
--- letterboxd/movies/views.py
+++ letterboxd/movies/views.py
@@ -33,3 +33,35 @@ class CustomLogoutView(LogoutView):
     def get(self, request, *args, **kwargs):
         response = super().get(request, *args, **kwargs)
         return redirect('home')
+    
+
+from .forms import CustomUserCreationForm
+
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = CustomUserCreationForm()
+
+    return render(request, 'registration/register.html', {'form': form})
+
+
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+from .forms import CustomUserCreationForm
+
+class RegisterView(CreateView):
+    template_name = 'registration/register.html'
+    form_class = CustomUserCreationForm
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        form.save()
+        return response
+
+    def get_success_url(self):
+        return reverse_lazy('login')
